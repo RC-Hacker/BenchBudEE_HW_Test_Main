2e9eb48b0474b3f6796b457602a60fa04885ae72
RC-Hacker <tpspam1@gmail.com>
2014-06-09 23:04:36 -0500

    Initial PWM Test Added
    Added simple PWM test that cycles through 5 PWM levels

--- fanModuleTest.ino
+++ fanModuleTest.ino
@@ -1,7 +1,7 @@
 // BenchBudEE HW Checkout Program
 //
 // fanModuleTest.ino --
-//  Fan Module Test suit
+// Fan Module Test suit
 //
 
 #include "BenchBudEePins.h"
@@ -9,12 +9,12 @@
 #include "menuUtility.h"
 
 // Declare prototypes for tests here --
-//  These are dummy examples and should be replaced.
 //
 static void fanModuleDacStairstep(void);
+static void fanModulePwmLevels(void);
 
 // fanModuleTestMain --
-//  This is the main entry point for the Fan Module Test menu
+// This is the main entry point for the Fan Module Test menu
 //
 void fanModuleTestMain(void)
 {
@@ -22,6 +22,7 @@ void fanModuleTestMain(void)
   // See menuUtility.h for details.
   const menuLine fanMenu[] = {
   {1, "Start DAC Stairstep Test", fanModuleDacStairstep},
+  {2, "PWM Levels Test", fanModulePwmLevels},
   };
 
   // No need to change anything else in this function --
@@ -30,17 +31,17 @@ void fanModuleTestMain(void)
 
 static void fanModuleDacStairstep(void)
 {
-  Mcp48xx  dac(FAN_DAC_SELECT_PIN,
+  Mcp48xx dac(FAN_DAC_SELECT_PIN,
                Mcp48xx::GAIN_MODE_TWO_X,
                FAN_LDAC_PIN,
                FAN_SHDN_PIN);
-  int      dacControl  = 0;
+  int dacControl = 0;
   
   // TODO: Replace this after FAN class is created --
-  // Make sure the FAN module is configured for constant current.
+  // Configure the FAN module for constant current.
   //pinMode(FAN_MODE_PIN, OUTPUT);
   //digitalWrite(FAN_MODE_PIN, LOW);
-  // Make sure FAN PWM is low so PWM MOSFET is off.
+  // Set FAN PWM low initally so PWM MOSFET is off.
   pinMode(FAN_PWM_PIN, OUTPUT);
   digitalWrite(FAN_PWM_PIN, LOW);
   // End TODO: --
@@ -49,18 +50,18 @@ static void fanModuleDacStairstep(void)
   
   dac.on();
 
-  do 
+  do
   {
     if (dacControl > MCP48XX_DAC_MAX)
       dacControl = 0;
     
-    if (dac.set(dacControl) !=  Mcp48xx::DAC_OK)
+    if (dac.set(dacControl) != Mcp48xx::DAC_OK)
     {
-      Serial.println("Error setting DAC control value.  Exiting test");
+      Serial.println("Error setting DAC control value. Exiting test");
       break;
     }
     
-    dacControl +=  64;
+    dacControl += 64;
     delay (100);
     
   } while (Serial.available() == 0);
@@ -72,5 +73,58 @@ static void fanModuleDacStairstep(void)
   
 }
 
-
+// TODO: Rework this test after FAN class is created --
+//
+static void fanModulePwmLevels(void)
+{
+  Mcp48xx dac(FAN_DAC_SELECT_PIN,
+               Mcp48xx::GAIN_MODE_TWO_X,
+               FAN_LDAC_PIN,
+               FAN_SHDN_PIN);
   
+  // Configure the FAN module for PWM Mode.
+  //pinMode(FAN_MODE_PIN, OUTPUT);
+  //digitalWrite(FAN_MODE_PIN, HIGH);
+  // Set FAN PWM low initally so PWM MOSFET is off.
+  pinMode(FAN_PWM_PIN, OUTPUT);
+  digitalWrite(FAN_PWM_PIN, LOW);
+  
+  // Setup the DAC ouput --
+  //
+  dac.on();
+  if (dac.set(64) != Mcp48xx::DAC_OK)
+  {
+    Serial.println("Error setting DAC control value. Exiting test");
+  }
+  
+  else
+  {
+    int pwmLevel = 0;
+    int pwmValue = 0;
+    
+    Serial.println("PWM 3 Level Test in progress. Enter any key to stop.");
+
+    do
+    {
+      pwmValue = map(pwmLevel, 0, 4, 0, 255); // Map values 0-4 to 0-255
+      analogWrite(FAN_PWM_PIN, pwmValue);
+      
+      if (++pwmLevel > 4)
+        pwmLevel = 0;
+      
+      Serial.print("PWM Value = ");
+      Serial.println(pwmValue);
+      Serial.println("");
+      
+      delay(2000);
+    } while (Serial.available() == 0);
+    
+    // Get input and discard it --
+    Serial.read();
+  }
+  
+  // Turn off the PWM MOSFET and DAC output before exiting --
+  //
+  digitalWrite(FAN_PWM_PIN, LOW);
+  dac.off();
+}
